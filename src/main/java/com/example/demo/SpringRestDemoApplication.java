1e9e3021b7b619091baaec7b6853a6c757f68dea
unknown <scs.durgesh@gmail.com>
2017-06-23 12:15:16 +0530

    Second commit

--- src/main/java/com/example/demo/SpringRestDemoApplication.java
+++ src/main/java/com/example/demo/SpringRestDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,6 +19,7 @@ import com.example.repository.EmployeeRepository;
 @EntityScan(basePackages={"com.example.entity"})
 @ComponentScan(basePackages={"com.example.repository","com.example.restController","com.example.service"})
 @EnableWebMvc
+@EnableEurekaClient
 public class SpringRestDemoApplication {
 
 	@Bean
